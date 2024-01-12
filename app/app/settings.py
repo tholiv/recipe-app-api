1c18415e590cebe2076d8dcf99294bc377d610d6
tholiv <tholiv@github.com>
2024-01-12 17:36:41 +1000

    Install correct Django version

--- app/app/settings.py
+++ app/app/settings.py
@@ -131,5 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectactular.openapi.AutoSchema',
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
